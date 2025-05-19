d8fc08884c5739a8054ee47edbb70aeb3b8c1c75
Ngoh1919 <aghetngoh97@yahoo.com>
2025-05-19 21:54:55 +0200

    Querying
    Added queries for the Api's

--- DATABASE/mysql.js
+++ DATABASE/mysql.js
@@ -12,3 +12,49 @@ const pool = mysql.createPool({
 
 const result = await pool.query('SELECT * FROM Customer');
 console.log(result[0]);
+
+
+// app.get("/this",async (req,res) => {
+    // const bidder = await getBidder();
+    // res.send(bidder) 
+    //})
+async function addBidder(bidder) {
+    const { bidID, auctionID, customerID, amount, bidTime } = bidder;
+    const sql = 'INSERT INTO Bid (bidID, auctionID, customerID, amount, bidTime) VALUES (?, ?, ?, ?, ?)';
+    const values = [name, email, phone];
+    const [result] = await pool.query(sql, values);
+    return result.insertId;
+} 
+
+async function getBidder(bidderID) {
+    const sql = 'SELECT * FROM Bid WHERE bidID = ?';
+    const [rows] = await pool.query(sql, [bidderID]);
+    return rows[0];
+}
+
+async function getAllBidders() {
+    const sql = 'SELECT * FROM Bid';
+    const [rows] = await pool.query(sql);
+    return rows[0];
+}
+
+async function getAllAuction() {
+    const sql = 'SELECT * FROM Auction'
+    const [rows] = await pool.query(sql);
+    return rows[0];
+
+}
+
+async function getAuction(auctionID) {
+    const sql = 'SELECT * FROM Auction WHERE auctionID = ?';
+    const [rows] = await pool.query(sql, [auctionID]);
+    return rows[0];
+}
+
+async function addAuction(auction) {
+    const { auctionID, auctionName, auctionDate, auctionLocation } = auction;
+    const sql = 'INSERT INTO Auction (auctionID, carID, startDate, endDate, startingPrice) VALUES (?, ?, ?, ?)';
+    const values = [auctionID, auctionName, auctionDate, auctionLocation];
+    const [result] = await pool.query(sql, values);
+    return result[0];
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,25 +3,27 @@ import { Router } from 'express';
 const authRouter = Router();
 
 authRouter.get('/', (req, res) => {
-    res.send('Auction list');
+    res.send({title: 'Get list for cars in auction'});
 });
 
 authRouter.get('/:auctionId', (req, res) => {
-    const { auctionId } = req.params;
-    res.send(`Auction ID: ${auctionId}`);
+    res.send(`Get a particular auction item with id}`);
 });
+
+authRouter.post('/:auctionId', (req, res) => {
+    res.send({title: 'Add a new auction item'});
+});
+
 authRouter.post('/:auctionId/bid', (req, res) => {
-    const { auctionId } = req.params;
-    const { bidAmount } = req.body;
-    res.send(`Bid of ${bidAmount} placed on auction ID: ${auctionId}`);
+    res.send({title: 'place a bid for a particular auction item'});
 });
-authRouter.get('/:auctionId/bids', (req, res) => {
-    const { auctionId } = req.params;
-    res.send(`Bids for auction ID: ${auctionId}`);
+authRouter.get('/:auctionId/bids', (req, res) => {    
+    res.send({title: 'Get all bids for a particular auction'});
 }); 
-authRouter.post('/auctionID', (req, res) => {
-    const { auctionItem } = req.body;
-    res.send(`Auction item created: ${auctionItem}`);
+authRouter.post('/:auctionId/member', (req, res) => {
+    res.send({title: 'Add a new auction member'});
 });
 
 export default authRouter;
+
+// auth == auc :)
